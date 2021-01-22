8390fd03902ed74c02ede7bc9b8c05d2a0ab8491
Juan Camilo Rico <juancarico92@gmail.com>
2021-01-22 12:11:04 -0500

    feat: favicon and page title updated

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Diamond'
+        return args
+      })
+  },
   css: {
     loaderOptions: {
       sass: {
